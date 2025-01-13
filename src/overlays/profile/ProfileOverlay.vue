7cbaed2a24c1bf33dbf7b474397052a67de7982d
papertek <littlerock894@gmail.com>
2025-01-12 19:31:44 -0600

    fix errors

--- src/overlays/profile/ProfileOverlay.vue
+++ src/overlays/profile/ProfileOverlay.vue
@@ -8,7 +8,7 @@ import Toast from '@/utils/Toast';
 
 import TitleBar from '../TitleBar.vue';
 import PopupOverlay from '../popup/PopupOverlay.vue';
-import Button from '@/components/Button.vue';
+import Button from '@/components/buttons/Button.vue';
 
 const content = ref();
 
