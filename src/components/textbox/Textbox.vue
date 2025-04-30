c00f634ca9f8303726c94cd75fbdc070d147a080
papertek <littlerock894@gmail.com>
2025-04-30 13:49:32 -0500

    new textbox design

--- src/components/textbox/Textbox.vue
+++ src/components/textbox/Textbox.vue
@@ -5,6 +5,7 @@ import { PhTextBolder, PhTextItalic, PhTextH, PhTextUnderline, PhTextStrikethrou
 
 import TextboxIcon from './TextboxIcon.vue';
 import TextboxActions from '@/utils/TextboxActions';
+import { stringify } from 'postcss';
 
 const props = defineProps({
 	isEditor: {
@@ -58,6 +59,14 @@ const props = defineProps({
 	simple: {
 		type: Boolean,
 		default: false
+	},
+	showAvatar: {
+		type: Boolean,
+		default: false
+	},
+	avatarUrl: {
+		type: String,
+		default: ''
 	}
 });
 
@@ -136,56 +145,70 @@ const handleTablesClick = () => {
 
 <template>
 	<div class="h-auto w-full flex flex-col rounded-xl bg-background-1 p-2 gap-2">
-		<div class="w-full flex gap-2">
+
+		<!-- profile pic and textbox -->
+		<div class="flex flex-row gap-2 w-full">
+			<img v-if="showAvatar" class="rounded-lg object-fill max-h-16 my-auto" :src="avatarUrl" alt="avatar" />
 			<textarea
-				class="h-10 w-full resize-none overflow-hidden rounded-lg bg-background-1 px-3 py-1 text-lg outline-none ring-background-4 focus:ring-2"
+				class="h-16 w-full resize-none overflow-hidden rounded-lg bg-background-1 px-3 py-1 text-lg outline-none ring-background-4 focus:ring-2"
 				:placeholder="placeholderText" @input="handleInput" @keydown="handleKeydown" :disabled="beDisabled"
 				:id="`${boxName}-textbox`" :value="value" ref="textareaRef" />
-
-			<button v-if="!simple"
-				:class='"m-auto flex size-10 items-center justify-center rounded-lg bg-background-3 p-1 cursor-" + `${beDisabled ? "deny" : "pointer"}`'
-				id="submit" @click="handleSubmit" :disabled="beDisabled">
-				<Component :is="submitIcon" :size="20" :disabled="beDisabled" :class="submitIconClasses"></Component>
-			</button>
-			<button v-if="isReply && !simple"
-				:class='"m-auto flex size-10 items-center justify-center rounded-lg bg-background-3 p-1 cursor-" + `${beDisabled ? "deny" : "pointer"}`'
-				id="cancel" @click="handleCancel" :disabled="beDisabled">
-				<Component :is="PhX" :size="20" :disabled="beDisabled" :class="submitIconClasses"></Component>
-			</button>
 		</div>
-		<div v-if="!simple" class="max-w-fit flex flex-wrap px-2 gap-2 bg-background-3 rounded-lg py-2">
-			<div id="formatting" class="w-auto flex gap-3">
-				<TextboxIcon :icon="PhTextBolder" :title="`Bold Text`" @click="handleBoldClick"
-					:disabled="beDisabled" />
-				<TextboxIcon :icon="PhTextItalic" :title="`Italic Text`" @click="handleItalicClick"
-					:disabled="beDisabled" />
-				<TextboxIcon v-if="isEditor" :icon="PhTextH" :title="`Header Text`" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhTextUnderline" :title="`Underlined Text`" @click="handleUnderlineClick"
-					:disabled="beDisabled" />
-				<TextboxIcon :icon="PhTextStrikethrough" :title="`Strikethrough Text`" @click="handleStrikethroughClick"
-					:disabled="beDisabled" />
+
+		<!-- fucking icons and send button -->
+		<div class="flex flex-row w-full justify-between">
+
+			<!-- icons -->
+			<div v-if="!simple" class="max-w-fit flex flex-wrap px-2 gap-2 bg-background-3 rounded-lg py-2">
+				<div id="formatting" class="w-auto flex gap-3">
+					<TextboxIcon :icon="PhTextBolder" :title="`Bold Text`" @click="handleBoldClick"
+						:disabled="beDisabled" />
+					<TextboxIcon :icon="PhTextItalic" :title="`Italic Text`" @click="handleItalicClick"
+						:disabled="beDisabled" />
+					<TextboxIcon v-if="isEditor" :icon="PhTextH" :title="`Header Text`" :disabled="beDisabled" />
+					<TextboxIcon :icon="PhTextUnderline" :title="`Underlined Text`" @click="handleUnderlineClick"
+						:disabled="beDisabled" />
+					<TextboxIcon :icon="PhTextStrikethrough" :title="`Strikethrough Text`"
+						@click="handleStrikethroughClick" :disabled="beDisabled" />
+				</div>
+				<div class="h-auto w-0.5" style="background: rgba(255, 255, 255, 15%);"></div>
+				<div id="list" class="w-auto flex gap-3">
+					<TextboxIcon :icon="PhListBullets" :title="`Bullet List`" @click="handleListClick"
+						:disabled="beDisabled" />
+					<TextboxIcon :icon="PhListNumbers" :title="`Number List`" @click="handleNumListClick"
+						:disabled="beDisabled" />
+					<TextboxIcon v-if="isEditor" :icon="PhListChecks" :title="`Checklist`" :disabled="beDisabled" />
+				</div>
+				<div class="h-auto w-0.5" style="background: rgba(255, 255, 255, 15%);"></div>
+				<div id="misc" class="w-auto flex gap-3">
+					<TextboxIcon :icon="PhQuotes" :title="`Blockquote`" @click="handleQuotesClick"
+						:disabled="beDisabled" />
+					<TextboxIcon v-if="isEditor" :icon="PhCodeSimple" :title="`Code`" :disabled="beDisabled" />
+					<TextboxIcon v-if="isEditor" :icon="PhHighlighter" :title="`Highlight`" :disabled="beDisabled" />
+					<TextboxIcon :icon="PhSelection" :title="`Spoiler`" @click="handleSpoilerClick"
+						:disabled="beDisabled" />
+					<TextboxIcon v-if="isEditor" :icon="PhGridFour" :title="`Table`" @click="handleTablesClick"
+						:disabled="beDisabled" />
+					<TextboxIcon :icon="PhLinkSimple" :title="`Link [Text](URL)`" @click="handleLinkClick"
+						:disabled="beDisabled" />
+					<TextboxIcon :icon="PhImage" :title="`Image ![Text](URL)`" @click="handleImageClick"
+						:disabled="beDisabled" />
+				</div>
 			</div>
-			<div class="h-auto w-0.5" style="background: rgba(255, 255, 255, 15%);"></div>
-			<div id="list" class="w-auto flex gap-3">
-				<TextboxIcon :icon="PhListBullets" :title="`Bullet List`" @click="handleListClick"
-					:disabled="beDisabled" />
-				<TextboxIcon :icon="PhListNumbers" :title="`Number List`" @click="handleNumListClick"
-					:disabled="beDisabled" />
-				<TextboxIcon v-if="isEditor" :icon="PhListChecks" :title="`Checklist`" :disabled="beDisabled" />
-			</div>
-			<div class="h-auto w-0.5" style="background: rgba(255, 255, 255, 15%);"></div>
-			<div id="misc" class="w-auto flex gap-3">
-				<TextboxIcon :icon="PhQuotes" :title="`Blockquote`" @click="handleQuotesClick" :disabled="beDisabled" />
-				<TextboxIcon v-if="isEditor" :icon="PhCodeSimple" :title="`Code`" :disabled="beDisabled" />
-				<TextboxIcon v-if="isEditor" :icon="PhHighlighter" :title="`Highlight`" :disabled="beDisabled" />
-				<TextboxIcon :icon="PhSelection" :title="`Spoiler`" @click="handleSpoilerClick"
-					:disabled="beDisabled" />
-				<TextboxIcon v-if="isEditor" :icon="PhGridFour" :title="`Table`" @click="handleTablesClick"
-					:disabled="beDisabled" />
-				<TextboxIcon :icon="PhLinkSimple" :title="`Link [Text](URL)`" @click="handleLinkClick"
-					:disabled="beDisabled" />
-				<TextboxIcon :icon="PhImage" :title="`Image ![Text](URL)`" @click="handleImageClick"
-					:disabled="beDisabled" />
+
+			<!-- the send button and cancel button -->
+			<div class="flex flex-row gap-2">
+				<button v-if="!simple"
+					:class='"flex size-10 items-center justify-center rounded-lg bg-background-3 p-2 cursor-" + `${beDisabled ? "deny" : "pointer"}`'
+					id="submit" @click="handleSubmit" :disabled="beDisabled">
+					<Component :is="submitIcon" :size="24" :disabled="beDisabled" :class="submitIconClasses">
+					</Component>
+				</button>
+				<button v-if="isReply && !simple"
+					:class='"flex size-10 items-center justify-center rounded-lg bg-background-3 p-2 cursor-" + `${beDisabled ? "deny" : "pointer"}`'
+					id="cancel" @click="handleCancel" :disabled="beDisabled">
+					<Component :is="PhX" :size="24" :disabled="beDisabled" :class="submitIconClasses"></Component>
+				</button>
 			</div>
 		</div>
 	</div>
